ef7d378242faa8b94e4ac17aaf7b730a877e8dda
Tom <tapontikes@gmail.com>
2023-05-31 13:25:31 -0400

    ping server

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,19 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const cron = require('node-cron');
+const os = require('os');
+const axios = require('axios')
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+cron.schedule('*/10 * * * *', () => {
+    axios.get("http://" + os.hostname() + ":" + app.get('port')).then(res => {
+        console.log(res.data);
+    });
+});
 
 app.use(cors())
 app.use(logger('dev'));
@@ -18,12 +27,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res) {
+app.use((req, res) => {
     res.send("Error").status(404);
 });
 
 // error handler
-app.use(function (err, req, res) {
+app.use((err, req, res) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
